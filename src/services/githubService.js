babdb8d7753a709206e2268419469d1a89bdc338
Vandana-Saharan <vandanasaharan11@gmail.com>
2026-05-30 03:49:16 +0530

    add api request header

--- src/services/githubService.js
+++ src/services/githubService.js
@@ -1,7 +1,12 @@
 const axios = require("axios");
 
 const fetchGithubProfile = async (username) => {
-  const response = await axios.get(`https://api.github.com/users/${username}`);
+  const response = await axios.get(`https://api.github.com/users/${username}`, {
+    headers: {
+      "Accept": "application/vnd.github+json",
+      "User-Agent": "github-profile-analyzer-api",
+    },
+  });
 
   return response.data;
 };
